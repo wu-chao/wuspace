aa9d393f0703211c68fcb920b46eee4b45b66f1c
wu_chao <chao726@126.com>
2017-06-10 19:03:38 +0800

    initial commit.

--- blog/src/main/java/com/wuspace/controller/BlogController.java
+++ blog/src/main/java/com/wuspace/controller/BlogController.java
@@ -1,24 +1,16 @@
 package com.wuspace.controller;
 
-import com.wuspace.domain.Blog;
-import com.wuspace.domain.mapper.BlogMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Set;
-
 @Controller
 public class BlogController {
 
-    @Autowired
-    private BlogMapper blogMapper;
 
     @RequestMapping(value = {"/", "/blogs"}, method = RequestMethod.GET)
     public String index(Model model) {
-        Set<Blog> blogs = blogMapper.findAllByOrderByCreatedAtDesc();
 
         return "blogs/index";
     }
---
+++ javautil/src/main/java/com/wuspace/util/RegexpUtils.java
@@ -0,0 +1,62 @@
+package com.wuspace.util;
+
+import org.apache.oro.text.regex.*;
+
+public class RegexpUtils {
+
+    /**
+     * 大陆手机号码 11 位数，匹配格式：前三位固定格式 + 后 8 位任意数
+     * 此方法中前三位格式有：
+     * 13 + 任意数
+     * 15 + 除 4 的任意数
+     * 18 + 除 1 和 4 的任意数
+     * 17 + 除 9 的任意数
+     * 147
+     */
+    public static final String PRC_PHONE_REGEXP = "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";
+
+    /**
+     * 1、微信号是微信的唯一凭证，只能设置 1 次；
+     * 2、可使用 6-20 个字母、数字、下划线和减号；
+     * 3、必须以字母开头（字母不区分大小写）；
+     * 4、不支持设置中文。
+     */
+    public static final String WECHAT_REGEXP = "^[a-zA-Z]{1}[-_a-zA-Z0-9]{5,19}$";
+
+    /**
+     * 18 位身份证号码各位的含义:
+     * 1-2 位省、自治区、直辖市代码；
+     * 3-4 位地级市、盟、自治州代码；
+     * 5-6 位县、县级市、区代码；
+     * 7-14 位出生年月日，比如 19670401 代表 1967 年 4 月 1 日；
+     * 15-17 位为顺序号，其中 17 位（倒数第二位）男为单数，女为双数；
+     * 18 位为校验码，0-9 和 X。作为尾号的校验码，是由把前十七位数字带入统一的公式计算出来的，计算的结果是 0-10，
+     * 如果某人的尾号是 0－9，都不会出现 X，但如果尾号是 10，那么就得用 X 来代替，因为如果用 10 做尾号，那么此人的身份证就变成了 19 位。X 是罗马数字的 10，用 X 来代替 10。
+     */
+    public static final String ID_REGEXP = "^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9]|X)$";
+
+    /**
+     * 邮编（6位纯数字）
+     */
+    public static final String ZIP_REGEXP = "^[0-9]{6}$";
+
+    /**
+     * IP
+     */
+    public static final String IP_REGEXP = "(25[0-5]|2[0-4]\\\\d|1\\\\d{2}|[1-9]?\\\\d)(\\\\.(25[0-5]|2[0-4]\\\\d|1\\\\d{2}|[1-9]?\\\\d)){3}";
+
+
+    public static boolean isMatched(String source, String regexp) {
+        // 依赖：https://mvnrepository.com/artifact/org.apache.oro/com.springsource.org.apache.oro
+        PatternCompiler patternCompiler = new Perl5Compiler();
+        PatternMatcher patternMatcher = new Perl5Matcher();
+        try {
+            Pattern pattern = patternCompiler.compile(regexp);
+            return patternMatcher.contains(source, pattern);
+        } catch (MalformedPatternException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
