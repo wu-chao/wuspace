07847af4d7802afc9581f8aa5a13465ee0f45df2
wu_chao <chao726@foxmail.com>
2017-07-23 14:30:31 +0800

    init admin.

--- admin/src/main/java/com/wuspace/controller/AdminIndexController.java
+++ admin/src/main/java/com/wuspace/controller/AdminIndexController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminIndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/admin")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/admin/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/admin/index2")
+    public String index2() {
+        return "index2";
     }
 
     @RequestMapping("/calendar")
