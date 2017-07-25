330380229cbc2325bcae5e4a902950c194e1e0db
chao <chao@gmx.com>
2017-07-25 22:50:00 +0800

    initial commit.

--- blog/src/main/java/com/wuspace/domain/BlogRepository.java
+++ blog/src/main/java/com/wuspace/domain/BlogRepository.java
@@ -2,9 +2,6 @@ package com.wuspace.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by wu-chao on 7/23/17.
- */
 public interface BlogRepository extends JpaRepository<Blog, Long> {
 
 
