345a184c5714c0d343c196d3673cc1f3ad03fd41
wuchao <chao@gmx.com>
2018-05-16 19:16:19 +0800

    initial commit.

--- blog/src/test/java/com.wuspace.util/CollectionsTest.java
+++ blog/src/test/java/com.wuspace.util/CollectionsTest.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-public class CollectionTest {
+public class CollectionsTest {
 
     ///////////////////////// list new /////////////////////////
     @Test
