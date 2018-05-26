65cc3cc8429fa65f59d3c8053a9a5d57abb8156f
wuchao <chao@gmx.com>
2018-05-26 13:47:32 +0800

    initial commit.

--- blog/src/main/java/com/wuspace/util/JsonXMLUtils.java
+++ blog/src/main/java/com/wuspace/util/JsonXMLUtils.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import java.io.IOException;
 
@@ -16,10 +18,12 @@ public abstract class JsonXMLUtils {
      * @return
      */
     public static String obj2json(Object obj) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-        mapper.registerModule(new Jdk8Module());
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
         try {
             return mapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
@@ -29,16 +33,19 @@ public abstract class JsonXMLUtils {
     }
 
     /**
-     * json字符串解析成对象
+     * json 字符串解析成对象
      *
      * @param json
      * @param clazz
      * @return
      */
     public static Object json2obj(String json, Class clazz) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        mapper.findAndRegisterModules();
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
         try {
             return mapper.readValue(json, clazz);
         } catch (IOException e) {
