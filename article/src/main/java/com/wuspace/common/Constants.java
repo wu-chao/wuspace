555c9f18c8fb1cccd34e65786fd5d6f1f52c04dc
wuchao <chao@gmx.com>
2018-05-31 12:05:34 +0800

    initial commit.

--- article/src/main/java/com/wuspace/common/Constants.java
+++ article/src/main/java/com/wuspace/common/Constants.java
@@ -3,6 +3,7 @@ package com.wuspace.common;
 public final class Constants {
 
     public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
 
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String ANONYMOUS_USER = "anonymoususer";
@@ -12,4 +13,5 @@ public final class Constants {
     private Constants() {
         throw new RuntimeException();
     }
+
 }
